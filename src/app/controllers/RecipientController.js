3771f384c60da1dd233e30d03123157566b379e9
Alexandre Feio <alefeio@gmail.com>
2020-03-06 03:28:26 -0300

    Criação, edição e autenticação de usuário admin; e criação e edição de destinatário

---
+++ src/app/controllers/RecipientController.js
@@ -0,0 +1,48 @@
+import * as Yup from 'yup';
+import Recipient from '../models/Recipient';
+
+class RecipientController {
+  async store(req, res) {
+    const schema = Yup.object().shape({
+      name: Yup.string().required(),
+      rua: Yup.string().required(),
+      numero: Yup.number().required(),
+      complemento: Yup.string(),
+      estado: Yup.string().required(),
+      cidade: Yup.string().required(),
+      cep: Yup.string().required(),
+    });
+
+    if (!(await schema.isValid(req.body))) {
+      return res.status(400).json({ erro: 'Falha na validação!' });
+    }
+
+    const recipient = await Recipient.create(req.body);
+
+    return res.json(recipient)
+  }
+
+  async update(req, res) {
+    const schema = Yup.object().shape({
+      name: Yup.string(),
+      rua: Yup.string(),
+      numero: Yup.number(),
+      complemento: Yup.string(),
+      estado: Yup.string(),
+      cidade: Yup.string(),
+      cep: Yup.string(),
+    });
+
+    if (!(await schema.isValid(req.body))) {
+      return res.status(400).json({ erro: 'Falha na validação!' });
+    }
+
+    const recipient = await Recipient.findByPk(req.body.id);
+
+    const up = await recipient.update(req.body)
+
+    res.json(up)
+  }
+}
+
+export default new RecipientController()
---
+++ src/config/auth.js
@@ -0,0 +1,4 @@
+export default {
+  secret: 'fcbd6597051f5b8182a274d8f69c13b3',
+  expiresIn: '7d'
+}
